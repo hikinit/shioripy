38825e6b9d647e44ca5735d5b6464e75c9487370
hikinit <hikinit@pm.me>
2021-01-27 17:41:58 +0700

    Add authentication for series mutation

--- series/tests/conftest.py
+++ series/tests/conftest.py
@@ -33,6 +33,19 @@ def client_query(client):
     return func
 
 
+@pytest.fixture
+def authenticated_client_query(client, django_user_model):
+    username = "user1"
+    password = "bar"
+    django_user_model.objects.create_user(username=username, password=password)
+    client.login(username=username, password=password)
+
+    def func(*args, **kwargs):
+        return graphql_query(*args, **kwargs, client=client)
+
+    return func
+
+
 urlpatterns = [
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
